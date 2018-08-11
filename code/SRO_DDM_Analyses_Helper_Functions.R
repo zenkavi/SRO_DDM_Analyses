8c5ec8d5e6336b5f0802795165fc81716eeced92
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-08-11 12:54:20 -0700

    debugging

--- code/SRO_DDM_Analyses_Helper_Functions.R
+++ code/SRO_DDM_Analyses_Helper_Functions.R
@@ -4,6 +4,8 @@ library(lme4)
 library(rmarkdown)
 library(DT)
 
+theme_set(theme_bw())
+
 render_this <- function(){rmarkdown::render('/Users/zeynepenkavi/Dropbox/PoldrackLab/SRO_DDM/code/SRO_DDM_Analyses.Rmd', output_dir = '/Users/zeynepenkavi/Dropbox/PoldrackLab/SRO_DDM/output/reports', html_notebook(toc = T, toc_float = T, toc_depth = 2, code_folding = 'hide'))}
 
 options(scipen = 1, digits = 4)
