6c6064acd9e9795969d38258487b18499f2bab1e
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-11-05 21:45:40 -0800

    added updated num_samples analyses and refactored

--- code/workspace_scripts/ddm_fitstat_data.R
+++ code/workspace_scripts/ddm_fitstat_data.R
@@ -8,7 +8,7 @@ process_fitstats = function(samples=c('t1_hierarchical', 't1_flat', 'refit_hiera
           tmp$subj_id = tmp$sub_id
           tmp = tmp[,-which(names(tmp)=="sub_id")]
         }
-        num_samples = as.numeric(gsub("[^\\d]+", "", f, perl=TRUE))
+        num_samples = as.numeric(gsub('.*_([0-9]+).*','\\1',f))
         tmp$num_samples = ifelse(is.na(num_samples), 500, num_samples)
         tmp$task_name = gsub(paste0("_",s,".*$"), "", f)
         tmp$sample = s
