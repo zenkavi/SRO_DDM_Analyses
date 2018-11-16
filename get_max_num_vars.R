37e40b33237b960cab0346746e11fb173eed11da
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-11-15 23:18:57 -0800

    script to get the max num of vars

---
+++ get_max_num_vars.R
@@ -0,0 +1,29 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+#Usage:
+#Rscript --vanilla get_max_num_vars.R file_name min_num_vars max_num_vars
+
+data = read.csv(args[1])
+min_num_vars = args[2]
+max_num_vars = args[3]
+
+out = data.frame(comb = NA, comb_det = NA)
+
+for(cur_num_vars in c(min_num_vars:max_num_vars)){
+  
+  combos = combn(ncol(data), cur_num_vars)
+  
+  for(i in c(1:dim(combos)[2])){
+    cur_comb = combos[,i]
+    cur_comb_det = det(cor(data[,cur_comb]))
+    tmp = c(cur_comb, cur_comb_det)
+    out = rbind(out, tmp)
+  }
+  
+}
+
+out = out[-1,]
+
+write.csv()
+
