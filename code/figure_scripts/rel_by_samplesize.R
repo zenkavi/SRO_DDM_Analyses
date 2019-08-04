fa4cf5039199a74a2f799a2b57e348128e5533e7
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-08-04 12:04:50 -0700

    added new report

--- code/figure_scripts/rel_by_samplesize.R
+++ code/figure_scripts/rel_by_samplesize.R
@@ -4,8 +4,17 @@ if(!exists('rel_df_sample_size_summary')){
   source('/Users/zeynepenkavi/Dropbox/PoldrackLab/SRO_DDM_Analyses/code/workspace_scripts/ddm_reldf_sample_size.R')
 }
 
+tmp = tmp %>%
+  ungroup()%>%
+  mutate(overall_difference = factor(overall_difference, levels = c("non-contrast", "condition", "contrast"), labels=c("Non-contrast", "Condition", "Contrast"))) %>%
+  filter(overall_difference != "Non-contrast")
+
 rel_df_sample_size_summary %>%
   na.omit() %>%
+  ungroup()%>%
+  mutate(ddm_raw = factor(ddm_raw, levels = c("raw", "ddm"), labels=c("Raw", "DDM")),
+         overall_difference = factor(overall_difference, levels = c("non-contrast", "condition", "contrast"), labels=c("Non-contrast", "Condition", "Contrast"))) %>%
+  filter(overall_difference != "Non-contrast") %>%
   ggplot(aes(factor(sample_size), mean_icc, color=ddm_raw))+
   geom_line(aes(group = dv), alpha = 0.1)+
   geom_line(data = tmp,aes(factor(sample_size),mean_icc, group=1), color="purple", size=1)+
@@ -15,6 +24,7 @@ rel_df_sample_size_summary %>%
   ylab("Mean reliability of 100 samples of size n")+
   xlab("Sample size")+
   ylim(-1,1)+
-  theme(legend.position = "none")
+  theme(legend.position = "none",
+        panel.grid=element_blank())
 
 ggsave(paste0('rel_by_samplesize.', out_device), device = out_device, path = fig_path, width = 10, height = 3.5, units = "in")
