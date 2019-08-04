a7a2c1af75e6b50aacd9d9c3eac11e27987c049f
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-08-04 10:44:16 -0700

    rendered new report

--- code/workspace_scripts/ddm_reldf_sample_size.R
+++ code/workspace_scripts/ddm_reldf_sample_size.R
@@ -1,5 +1,27 @@
-if(!exists('input_path') | input_path != 'https://raw.githubusercontent.com/zenkavi/SRO_DDM_Analyses/master/input/'){
-  input_path = 'https://raw.githubusercontent.com/zenkavi/SRO_DDM_Analyses/master/input/'
+if(!exists('input_path')){
+  if(from_gh){
+    input_path = 'https://raw.githubusercontent.com/zenkavi/SRO_DDM_Analyses/master/input/'
+  }else{
+    input_path = '/Users/zeynepenkavi/Dropbox/PoldrackLab/SRO_DDM_Analyses/input/'
+  }
+}
+
+if(!exists('measure_labels')){
+  if(from_gh){
+    ddm_workspace_scripts = 'https://raw.githubusercontent.com/zenkavi/SRO_DDM_Analyses/master/code/workspace_scripts/'
+    eval(parse(text = getURL(paste0(ddm_workspace_scripts,'hddm_fitstat_data.R'), ssl.verifypeer = FALSE)))
+  }else{
+    source('/Users/zeynepenkavi/Dropbox/PoldrackLab/SRO_DDM_Analyses/code/workspace_scripts/ddm_measure_labels.R')
+  }
+}
+
+if(!exists('sem')){
+  if(from_gh){
+    retest_helper_func = 'https://raw.githubusercontent.com/zenkavi/SRO_Retest_Analyses/master/code/helper_functions/'
+    eval(parse(text = getURL(paste0(retest_helper_func,'hddm_fitstat_data.R'), ssl.verifypeer = FALSE)))
+  }else{
+    source('/Users/zeynepenkavi/Dropbox/PoldrackLab/SRO_Retest_Analyses/code/helper_functions/sem.R')
+  }
 }
 
 rel_df_sample_size = read.csv(gzfile(paste0(input_path, 'rel_df_sample_size.csv.gz')))
